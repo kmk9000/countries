fbf9ae0b5d2cbff05e9637ba5c3024705a9756f8
kmk9000 <kallekoivuniemi@gmail.com>
2025-09-16 12:36:58 +0300

    display lists countries when under 10 results and shows country name (add more data next)

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 const Display = ({ countries }) => {
   console.log(countries);
-  return (
-    <>
-      <div>Loaded {countries.length} countries</div>
-    </>
-  );
+  // return <>Loaded {countries.length} countries</>;
+  if (countries.length > 10) {
+    return <>Too many matches ({countries.length})</>;
+  }
+  if (countries.length <= 10 && countries.length > 1) {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name.common}</li>
+        ))}
+      </ul>
+    );
+  }
+  if (countries.length === 1) {
+    const country = countries[0];
+    return (
+      <>
+        <h2>{country.name.common}</h2>
+      </>
+    );
+  }
 };
 
 export default Display;
