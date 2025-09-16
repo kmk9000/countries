0885669f279387a26a91460cd54157834f5dd5a7
kmk9000 <kallekoivuniemi@gmail.com>
2025-09-16 14:55:52 +0300

    minor cosmetic changes

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -18,14 +18,16 @@ const Display = ({ countries }) => {
 
     return (
       <>
-        <img src={country.flags.svg} style={{ width: "150px" }} />
         <h2>{country.name.common}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Area Code: {country.area}</p>
         <h3>Languages</h3>
         <ul>
           {Object.keys(country.languages).map((key) => (
             <li key={key}>{country.languages[key]}</li>
           ))}
         </ul>
+        <img src={country.flags.svg} style={{ width: "150px" }} />
       </>
     );
   }
