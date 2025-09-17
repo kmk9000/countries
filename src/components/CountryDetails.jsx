bb390655546b42f7da03f34b3ccf04bacdd44743
kmk9000 <kallekoivuniemi@gmail.com>
2025-09-17 14:27:12 +0300

    add weather component

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Weather from "./Weather";
+
 const CountryDetails = ({ country }) => {
+  const capital = country.capital?.[0];
   return (
     <>
       <h2>{country.name.common}</h2>
@@ -12,6 +15,8 @@ const CountryDetails = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.svg} style={{ width: "150px" }} />
+      <h3>Weather in {country.capital}</h3>
+      <Weather capital={capital} />
     </>
   );
 };
---
+++ src/components/Weather.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Weather = ({ capital }) => {
+  const [weather, setWeather] = useState(null);
+  const apiKey = import.meta.env.VITE_OPENWEATHER_KEY;
+
+  console.log("Weather component mounted");
+  console.log("Capital prop:", capital);
+  console.log("API key:", apiKey);
+
+  useEffect(() => {
+    if (!capital) return;
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${apiKey}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather:", error);
+      });
+  }, [capital, apiKey]);
+
+  if (!weather) return <p>Weather loading...</p>;
+
+  return (
+    <>
+      <p>Temperature: {weather.main.temp} °C</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt={weather.weather[0].description}
+      />
+      <p>Wind: {weather.wind.speed} m/s</p>
+    </>
+  );
+};
+
+export default Weather;
